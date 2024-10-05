707512e2ed668ee3f97b07ca81366bf58d60a039
becimester <becimester@gmail.com>
2024-10-05 23:57:26 +0100

    Added Linux compatibility

--- Assets/Plugins/Editor/Settings.cs
+++ Assets/Plugins/Editor/Settings.cs
@@ -7,7 +7,7 @@ public class Settings
 
     public static void ReloadBuildSettings()
     {
-        var settingsText = File.ReadAllText(Path.Combine(Application.dataPath, "../shipbreaker_settings.json"));
+        var settingsText = File.ReadAllText(Path.GetFullPath(Path.Combine(Application.dataPath, "..", "shipbreaker_settings.json")));
 
         buildSettings = null;
         try
@@ -33,48 +33,64 @@ public class Settings
 
     public static bool VerifyBuildSettings()
     {
+        // show all the applicable messages, not just one-by-one
+        bool valid = true;
+
         if(buildSettings.Author == null || buildSettings.Author == "")
         {
             Debug.LogError("Please provide an author name");
-            return false;
+            valid = false;
         }
         else if(buildSettings.ShipbreakerPath == null || buildSettings.ShipbreakerPath == "")
         {
-            Debug.LogError("Please provide a path to Hardspace: Shipbreaker");
-            return false;
+            Debug.LogError("Please provide the full path to Hardspace: Shipbreaker, and make sure it uses double backslashes (example: \"C:\\\\GOG Games\\\\Hardspace Shipbreaker\")");
+            valid = false;
         }
-        else if(buildSettings.ShipbreakerPath[2] != '\\')
+        
+        if (SystemInfo.operatingSystem.ToLower().Contains("linux"))
         {
-            Debug.LogError("Please escape the path to Hardspace: Shipbreaker (Double backslashes \\\\, not single \\)");
-            return false;
+            if (!buildSettings.ShipbreakerPath.Contains("/") || buildSettings.ShipbreakerPath.Contains("\\"))
+            {
+                Debug.LogError("Please provide the full Linux path to Hardspace: Shipbreaker (example: \"/home/myUser/games/gog_games/drive_c/GOG Games/Hardspace Shipbreaker\")");
+                valid = false;
+            }
+            if (buildSettings.WindowsShipbreakerPathOnLinux == null || buildSettings.WindowsShipbreakerPathOnLinux == "" || !buildSettings.WindowsShipbreakerPathOnLinux.Contains("\\"))
+            {
+                Debug.LogError("Please provide WindowsShipbreakerPathOnLinux as the Windows path seen by the game, and make sure it uses double backslashes (example: \"C:\\\\GOG Games\\\\Hardspace Shipbreaker\")");
+                valid = false;
+            }
         }
-        else if (!File.Exists(Path.Combine(buildSettings.ShipbreakerPath, "Shipbreaker.exe")))
+        else
+        {
+            if(buildSettings.ShipbreakerPath[2] != '\\')
+            {
+                Debug.LogError("Please escape the path to Hardspace: Shipbreaker (Double backslashes \\\\, not single \\)");
+                valid = false;
+            }
+        }
+        
+        if (!File.Exists(Path.Combine(buildSettings.ShipbreakerPath, "Shipbreaker.exe")))
         {
             Debug.LogError("Please provide a valid path to Hardspace: Shipbreaker (EXE not found)");
-            return false;
+            valid = false;
         }
         else if (!Directory.Exists(Path.Combine(buildSettings.ShipbreakerPath, "BepInEx")))
         {
             Debug.LogError("Please ensure BepInEx is installed (BepInEx folder not found)");
-            return false;
-        }
-        else if (!Directory.Exists(Path.Combine(buildSettings.ShipbreakerPath, "BepInEx")))
-        {
-            Debug.LogError("Please ensure BepInEx is installed (BepInEx folder not found)");
-            return false;
+            valid = false;
         }
         else if (!Directory.Exists(Path.Combine(buildSettings.ShipbreakerPath, "BepInEx", "plugins", "ModdedShipLoader")))
         {
             Debug.LogError("Please ensure the ModdedShipLoader is installed (plugin folder not found)");
-            return false;
+            valid = false;
         }
         else if (!Directory.Exists(Path.Combine(buildSettings.ShipbreakerPath, "BepInEx", "patchers", "ModdedShipLoaderPatcher")))
         {
             Debug.LogError("Please ensure the ModdedShipLoaderPatcher is installed (patcher folder not found)");
-            return false;
+            valid = false;
         }
 
-        return true;
+        return valid;
     }
 
     public static string GetAuthorName()
@@ -85,6 +101,7 @@ public class Settings
     public class BuildSettings
     {
         public string ShipbreakerPath;
+        public string WindowsShipbreakerPathOnLinux;
         public string Author;
     }
 }
